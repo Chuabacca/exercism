9ac4e5edde07ad6a54b365cc1d03ed82f3272bae
Chuabacca <jonathan.chua@dollarshaveclub.com>
2017-10-03 21:52:28 -0700

    Solved Gigasecond problem.

--- swift/gigasecond/Sources/Gigasecond.swift
+++ swift/gigasecond/Sources/Gigasecond.swift
@@ -1,1 +1,19 @@
 //Solution goes in Sources
+import Foundation
+
+class Gigasecond {
+    var description: String
+
+    init?(from: String) {
+        description = "will hold final date string"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        var birthDate = dateFormatter.date(from: from)
+        birthDate?.addTimeInterval(1000000000)
+        guard let output = birthDate else {
+            return
+        }
+       description = dateFormatter.string(from: output)
+    }
+}
