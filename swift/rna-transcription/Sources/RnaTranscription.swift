5b5d0a126791cddd153f5fc37cf04c2d4c5f6a0d
Chuabacca <jonathan.chua@dollarshaveclub.com>
2017-10-10 22:44:49 -0700

    RNA Transcription solution.

--- swift/rna-transcription/Sources/RnaTranscription.swift
+++ swift/rna-transcription/Sources/RnaTranscription.swift
@@ -1,1 +1,37 @@
 //Solution goes in Sources
+
+class Nucleotide {
+    var rna: String
+    var dna: String
+    enum RnaTranscription: Error {
+        case TranscriptionError
+    }
+    
+    init (_ dnaString: String) {
+        rna = ""
+        dna = dnaString
+    }
+    
+    func complementOfDNA() throws -> String {
+        for i in 0..<dna.count {
+            var nucleotide = Array(dna)[i]
+//            guard nucleotide == "G" || nucleotide == "C" || nucleotide == "T" || nucleotide == "A" else {
+//                throw RnaTranscription.TranscriptionError
+//            }
+            switch nucleotide {
+            case "G":
+                nucleotide = "C"
+            case "C":
+                nucleotide = "G"
+            case "T":
+                nucleotide = "A"
+            case "A":
+                nucleotide = "U"
+            default:
+                throw RnaTranscription.TranscriptionError
+            }
+            rna.append(nucleotide)
+        }
+        return rna
+    }
+}
